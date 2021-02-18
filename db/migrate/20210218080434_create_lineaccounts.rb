b79dcf658210d1541792cdb2327ab7e019ca85c4
chockty <75976150+chockty@users.noreply.github.com>
2021-02-18 17:28:06 +0900

    devise install 〜 DB migrate

---
+++ db/migrate/20210218080434_create_lineaccounts.rb
@@ -0,0 +1,9 @@
+class CreateLineaccounts < ActiveRecord::Migration[6.0]
+  def change
+    create_table :lineaccounts do |t|
+      t.string :uid, null: false
+      t.references :user, null: false, foreign_key: true
+      t.timestamps
+    end
+  end
+end
