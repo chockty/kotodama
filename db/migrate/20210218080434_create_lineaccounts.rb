9931b7e7adfa29dd1a45bd4c3059b758f22098b8
chockty <75976150+chockty@users.noreply.github.com>
2021-02-19 15:39:34 +0900

    db構築のためファイルを一時的に削除

--- db/migrate/20210218080434_create_lineaccounts.rb
+++
@@ -1,9 +0,0 @@
-class CreateLineaccounts < ActiveRecord::Migration[6.0]
-  def change
-    create_table :lineaccounts do |t|
-      t.string :uid, null: false
-      t.references :user, null: false, foreign_key: true
-      t.timestamps
-    end
-  end
-end
--- db/migrate/20210218080445_create_memos.rb
+++
@@ -1,9 +0,0 @@
-class CreateMemos < ActiveRecord::Migration[6.0]
-  def change
-    create_table :memos do |t|
-      t.text :content, null: false
-      t.references :user, null:false, foreign_key: true
-      t.timestamps
-    end
-  end
-end
