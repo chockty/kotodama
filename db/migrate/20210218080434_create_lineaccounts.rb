9c6740389f9ef9860409af197f44cb2c3b003ba9
chockty <75976150+chockty@users.noreply.github.com>
2021-02-20 14:38:26 +0900

    lineaccountモデルにnull許可

--- db/migrate/20210218080434_create_lineaccounts.rb
+++ db/migrate/20210218080434_create_lineaccounts.rb
@@ -2,7 +2,7 @@ class CreateLineaccounts < ActiveRecord::Migration[6.0]
   def change
     create_table :lineaccounts do |t|
       t.string :uid, null: false
-      t.references :user, null: false, foreign_key: true
+      t.references :user, foreign_key: true
       t.timestamps
     end
   end
