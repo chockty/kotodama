6837066387fdee940400183d7c4e4295fa03d26c
chockty <75976150+chockty@users.noreply.github.com>
2021-02-21 19:15:27 +0900

    Search.jsのループ箇所を一部修正

--- app/javascript/components/Search.js
+++ app/javascript/components/Search.js
@@ -17,13 +17,13 @@ class Search extends React.Component {
           <label key={"content-diary-label"+`${i}`} htmlFor={"content-diary"+`${i}`} className="diary-label"><a href={`/diaries/${this.props.result.result[0][i].id}`}>Edit</a></label>,
           <li key={"content-diary"+`${i}`} id={"content-diary"+`${i}`} className="diary-content">{this.props.result.result[0][i].content}</li>,
         )
+        }
       for(let i = 0; i < this.props.result.result[1].length; i++){
         memoList.push(
           <label key={"content-memo-label"+`${i}`} htmlFor={"content-memo"+`${i}`} className="memo-label"><a href={`/memos/${this.props.result.result[1][i].id}`}>Edit</a></label>,
           <li key={"content-memo"+`${i}`} id={"content-memo"+`${i}`} className="memo-content">{this.props.result.result[1][i].content}</li>,
         )
       }
-    }  
     }else if (this.props.result.result[0] != []){
       for(let i = 0; i < this.props.result.result[1].length; i++){
         diaryList.push(
