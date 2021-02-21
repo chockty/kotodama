63fa766bc724eb0cff354a0bb41803f030c4f69a
chockty <75976150+chockty@users.noreply.github.com>
2021-02-21 14:56:34 +0900

    LINE loginのエラーハンドリングに細部を変更
    原因：
    application.jsに「require("@rails/ujs").start()」がコメントアウトされていた。

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable,:omniauthable, omniauth_providers: [:line]
+         :recoverable, :rememberable, :validatable, :omniauthable, omniauth_providers: [:line]
   validates :nickname, presence: true
 
   has_one :lineaccount, dependent: :destroy
--- config/routes.rb
+++ config/routes.rb
@@ -5,13 +5,13 @@ Rails.application.routes.draw do
     registrations: 'users/registrations',
     sessions: 'users/sessions'
   }
+  root to: 'contents#top'
   resources :diaries, only:[:create, :show, :edit, :update]
   resources :memos, only:[:create, :show, :edit, :update]
   resources :users, only:[:show, :edit, :update] do
     resources :contents, only:[:index]
     post '/contents/search' => "contents#search"
   end
-  root to: 'contents#top'
   post '/callback' => 'webhooks#callback'
   post '/users/:user_id/contents' => 'contents#index'
 end
