fe1a7947d2da4cfb6acf573d926d503e67ca8917
chockty <75976150+chockty@users.noreply.github.com>
2021-02-24 19:43:30 +0900

    リマインド用のPOSTリクエストが来た際の条件分岐を作成

--- app/controllers/webhooks_controller.rb
+++ app/controllers/webhooks_controller.rb
@@ -7,65 +7,68 @@ class WebhooksController < ApplicationController
       config.channel_token = ENV["LINE_CHANNEL_TOKEN"]
     }
   end
-
+  
   def callback
     body = request.body.read
 
-    signature = request.env['HTTP_X_LINE_SIGNATURE']
-    unless client.validate_signature(body, signature)
-      halt 400, {'Content-Type' => 'text/plain'}, 'Bad Request'
-    end
+    if params[:remind] == "on"
+      user_lines = []
+      user_emails = []
+      functions = Function.where(line_remind: 1).or(Function.where(mail_remind: 1))
+      functions.each do |function|
+        if function.line_remind == 1
+          lineaccount = Lineaccount.find_by(user_id: function.user_id)
+          user_lines.push(lineaccount.uid)
+        elsif function.email_remind == 1
+          user = User.find(function.user_id)
+          user_emails.push(user.email)
+        end
+      end
+      if user_lines != []
+        create_broadcast(user_lines)
+      end
+      if user_emails != []
+        SampleMailer.sample_mailer.auto_broadcast(user_emails)
+      end
 
-    events = client.parse_events_from(body)
+    else
+      signature = request.env['HTTP_X_LINE_SIGNATURE']
+      unless client.validate_signature(body, signature)
+        halt 400, {'Content-Type' => 'text/plain'}, 'Bad Request'
+      end
 
-    events.each do |event|
-      case event
-      when Line::Bot::Event::Message
-        case event.type
-        when Line::Bot::Event::MessageType::Text
+      events = client.parse_events_from(body)
 
-          case event.message["text"]
-          when "日記"
-            if line_user = Lineaccount.where(uid: event["source"]["userId"]).limit(1)
-              if user = User.find(line_user[0].user.id)
-                diary = Diary.where(user_id: user.id).order(created_at: :desc).limit(2)
-                today = Date.today.to_s
-                if diary == []
-                  if user.function.diary_mode == 0 || user.function.diary_mode == 1
-                    if user.function.memo_mode == 1
-                      user.function.memo_mode = 0
-                      user.function.save
+      events.each do |event|
+        case event
+        when Line::Bot::Event::Message
+          case event.type
+          when Line::Bot::Event::MessageType::Text
+
+            case event.message["text"]
+            when "日記"
+              if line_user = Lineaccount.where(uid: event["source"]["userId"]).limit(1)
+                if user = User.find(line_user[0].user.id)
+                  diary = Diary.where(user_id: user.id).order(created_at: :desc).limit(2)
+                  today = Date.today.to_s
+                  if diary == []
+                    if user.function.diary_mode == 0 || user.function.diary_mode == 1
+                      if user.function.memo_mode == 1
+                        user.function.memo_mode = 0
+                        user.function.save
+                      end
+                      if user.function.diary_mode == 0
+                        user.function.diary_mode = 1
+                        user.function.save
+                      end
+                      2.times do |num|
+                        Diary.create(user_id: user.id, question: num+1)
+                      end
+                      reply_question(event)
                     end
-                    if user.function.diary_mode == 0
-                      user.function.diary_mode = 1
-                      user.function.save
-                    end
+                  elsif diary[0].content == nil && diary[1].content == nil
                     2.times do |num|
-                      Diary.create(user_id: user.id, question: num+1)
-                    end
-                    reply_question(event)
-                  end
-                elsif diary[0].content == nil && diary[1].content == nil
-                  2.times do |num|
-                    diary[num].destroy
-                    Diary.create(user_id: user.id, question: num+1)
-                  end
-                  if user.function.diary_mode == 0 || user.function.diary_mode == 1
-                    if user.function.memo_mode == 1
-                      user.function.memo_mode = 0
-                      user.function.save
-                    end
-                    if user.function.diary_mode == 0
-                      user.function.diary_mode = 1
-                      user.function.save
-                    end
-                    reply_question(event)
-                  # else
-                  #   reply_question(event)
-                  end
-                elsif diary[0].created_at.to_s.include?(today) == false
-                  if diary[0].content || diary[1].content
-                    2.times do |num|
+                      diary[num].destroy
                       Diary.create(user_id: user.id, question: num+1)
                     end
                     if user.function.diary_mode == 0 || user.function.diary_mode == 1
@@ -78,83 +81,102 @@ class WebhooksController < ApplicationController
                         user.function.save
                       end
                       reply_question(event)
+                    # else
+                    #   reply_question(event)
                     end
+                  elsif diary[0].created_at.to_s.include?(today) == false
+                    if diary[0].content || diary[1].content
+                      2.times do |num|
+                        Diary.create(user_id: user.id, question: num+1)
+                      end
+                      if user.function.diary_mode == 0 || user.function.diary_mode == 1
+                        if user.function.memo_mode == 1
+                          user.function.memo_mode = 0
+                          user.function.save
+                        end
+                        if user.function.diary_mode == 0
+                          user.function.diary_mode = 1
+                          user.function.save
+                        end
+                        reply_question(event)
+                      end
+                    end
+                  elsif (diary[0].content || diary[1].content) && diary[0].created_at.to_s.include?(today)
+                    reply_url(event)
                   end
-                elsif (diary[0].content || diary[1].content) && diary[0].created_at.to_s.include?(today)
-                  reply_url(event)
+                  # content_1が埋まっているが日付が昨日の場合を作る。
+                else
+                  reply_not_registered(event)
                 end
-                # content_1が埋まっているが日付が昨日の場合を作る。
               else
                 reply_not_registered(event)
               end
-            else
-              reply_not_registered(event)
-            end
 
-          when "メモ"
-            if line_user = Lineaccount.where(uid: event["source"]["userId"]).limit(1)
-              if user = User.find(line_user[0].user.id)
-                function = user.function
-                if function.diary_mode == 1 
+            when "メモ"
+              if line_user = Lineaccount.where(uid: event["source"]["userId"]).limit(1)
+                if user = User.find(line_user[0].user.id)
+                  function = user.function
+                  if function.diary_mode == 1 
+                      function.diary_mode = 0
+                      if function.memo_mode == 0
+                        function.memo_mode = 1
+                        function.save
+                        reply_memo_mode(event)
+                      elsif function.memo_mode == 1
+                        function.save
+                        reply_memo_mode(event)
+                      end
+                  elsif function.diary_mode == 0 && function.memo_mode == 1
+                    reply_memo_mode(event)
+                  elsif function.diary_mode == 0 && function.memo_mode == 0
+                    function.memo_mode =1
+                    function.save
+                    reply_memo_mode(event)
+                  end
+                else
+                  reply_not_registered(event)
+                end
+              else
+                reply_not_registered(event)
+              end
+            
+            # when ""
+            #   users_info = Lineaccount.all.where()
+
+            else
+              if line_user = Lineaccount.where(uid: event["source"]["userId"]).limit(1)
+                if user = User.find(line_user[0].user.id)
+                  function = user.function
+                  diary = Diary.where(user_id: user.id).order(created_at: :desc).limit(2)
+                  if function.diary_mode == 1 && function.memo_mode == 0 && diary[1].content == nil && diary[0].content == nil
+                    diary[1].content = event.message["text"]
+                    diary[1].save
+                    reply_Q1(event)
+                  elsif function.diary_mode == 1 && function.memo_mode == 0 && diary[1].content && diary[0].content == nil
+                    diary[0].content = event.message["text"]
+                    diary[0].save
                     function.diary_mode = 0
-                    if function.memo_mode == 0
-                      function.memo_mode = 1
-                      function.save
-                      reply_memo_mode(event)
-                    elsif function.memo_mode == 1
-                      function.save
-                      reply_memo_mode(event)
-                    end
-                elsif function.diary_mode == 0 && function.memo_mode == 1
-                  reply_memo_mode(event)
-                elsif function.diary_mode == 0 && function.memo_mode == 0
-                  function.memo_mode =1
-                  function.save
-                  reply_memo_mode(event)
+                    function.save
+                    reply_Q2(event)
+                  elsif function.diary_mode == 0 && function.memo_mode == 1
+                    Memo.create(content: event.message["text"], user_id: user.id)
+                    reply_Q2(event)
+                    function.memo_mode = 0
+                    function.save
+                  end
+                else
+                  reply_not_registered(event)
                 end
               else
                 reply_not_registered(event)
               end
-            else
-              reply_not_registered(event)
             end
-          
-          # when ""
-          #   users_info = Lineaccount.all.where()
 
-          else
-            if line_user = Lineaccount.where(uid: event["source"]["userId"]).limit(1)
-              if user = User.find(line_user[0].user.id)
-                function = user.function
-                diary = Diary.where(user_id: user.id).order(created_at: :desc).limit(2)
-                if function.diary_mode == 1 && function.memo_mode == 0 && diary[1].content == nil && diary[0].content == nil
-                  diary[1].content = event.message["text"]
-                  diary[1].save
-                  reply_Q1(event)
-                elsif function.diary_mode == 1 && function.memo_mode == 0 && diary[1].content && diary[0].content == nil
-                  diary[0].content = event.message["text"]
-                  diary[0].save
-                  function.diary_mode = 0
-                  function.save
-                  reply_Q2(event)
-                elsif function.diary_mode == 0 && function.memo_mode == 1
-                  Memo.create(content: event.message["text"], user_id: user.id)
-                  reply_Q2(event)
-                  function.memo_mode = 0
-                  function.save
-                end
-              else
-                reply_not_registered(event)
-              end
-            else
-              reply_not_registered(event)
-            end
           end
-
         end
       end
+      "OK"
     end
-    "OK"
   end
 
 
@@ -205,6 +227,14 @@ class WebhooksController < ApplicationController
       text: "メモ投稿\n自由に書いてみてください。"
     }
     client.reply_message(event['replyToken'], message)
+  end
+
+  def create_broadcast(user_lines)
+    message = {
+      type: 'text',
+      text: "こんばんは。今日はどんなことがありましたか？どんなことを感じましたか？あなたの思いを言葉にしてみてください。"
+    }
+    client.multicast(user_lines, message)
   end
 
 end
